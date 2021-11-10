c0c3a3b87139e6de22dcd7071164fbfb66b820d2
songyang <610467559@qq.com>
2021-11-10 21:44:52 +0800

    wq

--- day-three/index.ts
+++ day-three/index.ts
@@ -74,9 +74,66 @@ const parsed = map(['1', '2', '3'], (n) => parseInt(n))
 
 // Constraints 约束
 
+// exp1: 约束参数要有 length属性 属性值是一个number
+function longest<T extends { length: number}>(a: T, b: T) {
+	if (a.length >= b.length) {
+		return a
+	} else {
+		return b
+	}
+}
+
+const longerArray = longest([1, 2, 3], [1, 2])
+
+const longerString = longest('hello world', 'xyz')
+
+// Error! Number dont have a "length" property
+const notOk = longest(10, 200)
+
+/* 泛型都是关于用同一类型关联两个或多个值的！ */
+
+// Working with Constrained Values 使用的约束
+
+// exp1
+function mininumlength<T extends { length: number }>(obj: T, minnum: number): T {
+	if (obj.length >= minnum) {
+		return obj
+	}
+	return {length: minnum}
+}
+
+const arrMinnum = mininumlength([1, 2, 3], 6)
+
+console.log(arrMinnum.slice(0))
+
+// Specifying Type Arguments 指定类型参数
+function combine<T>(arr1: T[], arr2: T[]) : T[]{
+	return arr1.concat(arr2)
+}
+
+combine<number | string>([1, 2, 3], ['xyz'])
 
 
+// Guidelines for writing good generic function 编写良好的通用函数的指南
 
+
+// 尽可能的处理参数本身的类型
+function firstElement1<T>(arr: T[]): T {
+	return arr[0]
+}
+
+function firstElement2<T extends any[]>(arr: T) {
+	return arr[0]
+}
+
+// 类型参数的数量尽量少一些
+function filter1<T>(arr: T[], func: (arg: T) => boolean): T[] {
+	return arr.filter(func)
+}
+
+function filter2<T, Func extends (arg: T) => boolean>(arr: T[], func: Func): T[] {
+	return arr.filter(func)
+}
 
 
 
